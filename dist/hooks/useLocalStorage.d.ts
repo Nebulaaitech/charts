8e06e606ee0e85fd46e46b9fc249ebf28028d018
ne0c0der <217756616+ne0c0der@users.noreply.github.com>
2025-09-14 19:39:47 -0400

    Delete dist directory

--- dist/hooks/useLocalStorage.d.ts
+++
@@ -1,2 +0,0 @@
-export declare function useLocalStorage<T>(key: string, initialValue: T): [T, (value: T | ((val: T) => T)) => void, () => void];
-//# sourceMappingURL=useLocalStorage.d.ts.map
--- dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-export { useLocalStorage } from './hooks/useLocalStorage';
-export { useMediaQuery } from './hooks/useMediaQuery';
-export type LocalStorageHook<T> = [T, (value: T | ((val: T) => T)) => void, () => void];
-//# sourceMappingURL=index.d.ts.map
